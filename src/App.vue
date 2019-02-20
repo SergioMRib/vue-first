aff346e17aeb8e80b8a87f3a14933ae27bdad75c
Sérgio <sergio.m.rib@outlook.com>
2019-02-20 15:57:58 +0000

    These were experimentations that work
    Added a component of my own similar to HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoodBye mensagem="Esta é a minha mensagem"/>
+    <GoodBye mensagem="Esta é a OUTRA mensagem" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GoodBye from './components/MyOwn.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    GoodBye
   }
 }
 </script>
---
+++ src/components/MyOwn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="goodbye">
+        <p> {{mensagem}} </p>
+        <p> {{mensagem}} </p>
+        <p> {{mensagem}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GoodBye',
+    props: {
+        mensagem: String
+    }
+
+}
+</script>
+
+<style scoped>
+    .goodbye {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        color: green;
+        background-color: aquamarine;
+    }
+</style>
